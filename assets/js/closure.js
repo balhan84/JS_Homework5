6a56f855bb497d213227652585391d3eb9f6133b
balhan84 <balamutenkoanna@gmail.com>
2024-06-06 00:44:24 +0300

    Add recursion and closure

---
+++ assets/js/closure.js
@@ -0,0 +1,29 @@
+//task 1
+
+// function customCounter1(n) {
+//   let count = 0;
+
+//   function increased() {
+//     return (count += n);
+//   }
+//   return increased;
+// }
+// const myCounter = customCounter1(5);
+// console.log(myCounter());
+// console.log(myCounter());
+// console.log(myCounter());
+
+//task 2
+
+// function customCounter1(count, n) {
+//   //   let count = 0;
+
+//   function increased() {
+//     return (count += n);
+//   }
+//   return increased;
+// }
+// const myCounter = customCounter1(1, 5);
+// console.log(myCounter());
+// console.log(myCounter());
+// console.log(myCounter());
---
+++ assets/js/recursion.js
@@ -0,0 +1,38 @@
+//task 1
+
+function valid(base, exponent) {
+  if (typeof base !== "number") {
+    throw new TypeError("base must be number");
+  }
+  if (typeof exponent !== "number") {
+    throw new TypeError("exponent must be number");
+  }
+  if (
+    !Number.isInteger(exponent) ||
+    exponent < 0 ||
+    exponent > Number.MAX_SAFE_INTEGER
+  ) {
+    throw new RangeError("exponent must be nonnegative integer value");
+  }
+  return base ** exponent;
+}
+function pow(base, exponent) {
+  if (exponent === 1) {
+    return base;
+  }
+  return base * pow(base, exponent - 1);
+}
+
+try {
+  const powNumber = valid(3, 3);
+  console.log(powNumber);
+} catch (err) {
+  if (err instanceof RangeError) {
+    console.log("RangeError");
+  } else if (err instanceof TypeError) {
+    console.log("TypeError");
+  } else {
+    console.log("Error");
+    console.log("error:>>", err);
+  }
+}
